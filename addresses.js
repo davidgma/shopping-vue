f6ee436fb12c8a15c3b9b54772c0990be7de879a
davidgma <dmlh02@gmail.com>
2025-08-31 19:23:07 +0100

    a

--- addresses.js
+++ addresses.js
@@ -7,12 +7,17 @@ export const AddressesView = {
 	<div class="form-group">
 	
   <label>Spreadsheet ID (the long text between the /d/ and the /edit/ in the address)</label>
-		<input type="text" class="form-control" id="sheetId" required  name="sheetId">
+		<input type="text" class="form-control" id="sheetId"  name="sheetId">
+	</div>
+
+  <div class="form-group">
+		<label>Client Id</label>
+		<input type="text" class="form-control" id="clientId" name="clientId">
 	</div>
 
 	<div class="form-group">
-		<label>Range e.g. Sheet1!A1:C</label>
-		<input type="text" class="form-control" id="range" required name="range">
+		<label>Range for testing e.g. Sheet1!A1:C</label>
+		<input type="text" class="form-control" id="range" name="range">
 	</div>
 
 	<button type="submit" class="btn btn-success addresses-button">Submit</button>
